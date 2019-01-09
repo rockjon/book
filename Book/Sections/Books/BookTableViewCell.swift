8bffb60e513477817610a67f561e07b6240f538f
Javier Cruz Santiago <javiercruzsantiago@gmail.com>
2019-01-09 00:03:07 -0600

    [UPDATE] - Added first version of ExpandCollapseTableViews

--- Book/Sections/Books/BookTableViewCell.swift
+++ Book/Sections/Books/BookTableViewCell.swift
@@ -23,6 +23,10 @@ class BookTableViewCell: UITableViewCell {
     var labelPages: UILabel!
     var labelBookPages: UILabel!
     var buttonSeeMore: UIButton!
+    var labelDescription: UILabel!
+    var textViewDescription: UITextView!
+    var labelAbstract: UILabel!
+    var textViewAbstract: UITextView!
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -47,6 +51,25 @@ class BookTableViewCell: UITableViewCell {
         labelPages = UILabel.getCustomLabel(text: "Páginas:", font: .Bold, size: 16, color: .FONT_COLOR, alignment: .left)
         labelBookPages = UILabel.getCustomLabel(text: "NumberOfPages", font: .Regular, size: 16, color: .FONT_COLOR, alignment: .right)
         
+        buttonSeeMore = UIButton()
+        buttonSeeMore.setImage(UIImage(named: "icDetailDown"), for: .normal)
+        
+        labelDescription = UILabel.getCustomLabel(text: "Descripción", font: .Bold, size: 16, color: .FONT_COLOR, alignment: .left)
+        labelDescription.isHidden = true
+        
+        textViewDescription = UITextView()
+        textViewDescription.font = UIFont(name: FontType.Regular.rawValue, size: 12)
+        textViewDescription.isEditable = false
+        textViewDescription.isHidden = true
+        
+        labelAbstract = UILabel.getCustomLabel(text: "Abstracción", font: .Bold, size: 16, color: .FONT_COLOR, alignment: .left)
+        labelAbstract.isHidden = true
+        
+        textViewAbstract = UITextView()
+        textViewAbstract.font = UIFont(name: FontType.Regular.rawValue, size: 12)
+        textViewAbstract.isEditable = false
+        textViewAbstract.isHidden = true
+        
         infoView.addSubview(imageBookCover)
         infoView.addSubview(labelBookTitle)
         infoView.addSubview(bookStars)
@@ -56,6 +79,11 @@ class BookTableViewCell: UITableViewCell {
         infoView.addSubview(labelBookPublished)
         infoView.addSubview(labelPages)
         infoView.addSubview(labelBookPages)
+        infoView.addSubview(buttonSeeMore)
+        infoView.addSubview(labelDescription)
+        infoView.addSubview(textViewDescription)
+        infoView.addSubview(labelAbstract)
+        infoView.addSubview(textViewAbstract)
         
         infoView.addConstraintsWithFormat(format: "V:|-10-[v0(30)]", views: labelBookTitle)
         infoView.addConstraintsWithFormat(format: "H:|-150-[v0]-10-|", views: labelBookTitle)
@@ -81,9 +109,58 @@ class BookTableViewCell: UITableViewCell {
         infoView.addConstraintsWithFormat(format: "V:[v0]-5-[v1]", views: labelPublished,labelBookPages)
         infoView.addConstraintsWithFormat(format: "H:|-150-[v0]-10-|", views: labelBookPages)
         
+        infoView.addConstraintsWithFormat(format: "V:[v0][v1(30)]", views: labelBookPages,buttonSeeMore)
+        infoView.addConstraintsWithFormat(format: "H:[v0(40)]|", views: buttonSeeMore)
+        
+        infoView.addConstraintsWithFormat(format: "V:|-190-[v0(20)]", views: labelDescription)
+        infoView.addConstraintsWithFormat(format: "H:|-15-[v0]-15-|", views: labelDescription)
+        
+        infoView.addConstraintsWithFormat(format: "V:[v0]-10-[v1(80)]", views: labelDescription,textViewDescription)
+        infoView.addConstraintsWithFormat(format: "H:|-15-[v0]-15-|", views: textViewDescription)
+        
+        infoView.addConstraintsWithFormat(format: "V:[v0]-10-[v1(10)]", views: textViewDescription,labelAbstract)
+        infoView.addConstraintsWithFormat(format: "H:|-15-[v0]-15-|", views: labelAbstract)
+        
+        infoView.addConstraintsWithFormat(format: "V:[v0]-10-[v1(80)]", views: labelAbstract,textViewAbstract)
+        infoView.addConstraintsWithFormat(format: "H:|-15-[v0]-15-|", views: textViewAbstract)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+//MARK: Set Cell Data
+extension BookTableViewCell {
+    func setData (book: BookModel) {
+        labelBookTitle.text = book.Title
+        labelBookAuthor.text = ""
+        labelBookPublished.text = book.PublishDate?.components(separatedBy: "T")[0]
+        labelBookPages.text = "\(book.PageCount ?? 0)"
+        textViewDescription.text = book.Description
+        textViewAbstract.text = book.Excerpt
+    }
+}
+
+//MARK: Expand and Dismiss Logic
+extension BookTableViewCell {
+    func hideViews() {
+        hideOrShowView(view: labelDescription, hide: true)
+        hideOrShowView(view: textViewDescription, hide: true)
+        hideOrShowView(view: labelAbstract, hide: true)
+        hideOrShowView(view: textViewAbstract, hide: true)
+    }
+    
+    func showViews() {
+        hideOrShowView(view: labelDescription, hide: false)
+        hideOrShowView(view: textViewDescription, hide: false)
+        hideOrShowView(view: labelAbstract, hide: false)
+        hideOrShowView(view: textViewAbstract, hide: false)
+    }
+    
+    private func hideOrShowView(view: UIView, hide: Bool) {
+        UIView.transition(with: view, duration: 0.5, options: .transitionCrossDissolve, animations: {
+            view.isHidden = hide
+        })
     }
 }
--- Book/Sections/Books/BooksView.swift
+++ Book/Sections/Books/BooksView.swift
@@ -14,6 +14,10 @@ class BooksView: UITableView {
     private var viewFrame: CGRect!
     var books: [BookModel]! = []
     
+    var infoViewHeight: Int!
+    var expandedCellIndex: Int?
+    var expand: Bool?
+    
     override init(frame: CGRect, style: UITableView.Style) {
         super.init(frame: frame, style: style)
     }
@@ -23,7 +27,7 @@ class BooksView: UITableView {
         self.viewFrame = self.context.view.frame
         backgroundColor = .clear
         register(BookTableViewCell.self, forCellReuseIdentifier: BookTableViewCell.ID)
-        rowHeight = 200
+//        rowHeight = 200
         separatorStyle = .none
         allowsSelection = false
         delegate = self
@@ -77,11 +81,65 @@ extension BooksView: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: BookTableViewCell.ID, for: indexPath) as! BookTableViewCell
-        cell.infoView.frame = CGRect(x: 20, y: 15, width: viewFrame.width - 40, height: tableView.rowHeight - 30)
-        cell.labelBookTitle.text = books[indexPath.row].Title
-        cell.labelBookPublished.text = books[indexPath.row].PublishDate?.components(separatedBy: "T")[0]
-        cell.labelBookPages.text = "\(books[indexPath.row].PageCount ?? -1)"
-        return cell
+        tableView.rowHeight = 200
+        if indexPath.row == expandedCellIndex {
+            if expand ?? false {
+                let cell = BookTableViewCell(frame: CGRect(x: 0, y: 0, width: viewFrame.width, height: tableView.rowHeight))
+                cell.infoView.frame = CGRect(x: 20, y: 15, width: viewFrame.width - 40, height: tableView.rowHeight - 30)
+                cell.setData(book: books[indexPath.row])
+                cell.buttonSeeMore.tag = indexPath.row
+                cell.buttonSeeMore.addTarget(self, action: #selector(expandRow(sender:)), for: .touchUpInside)
+                tableView.rowHeight = 500
+                UIView.animate(withDuration: 0.5, animations: {
+                    cell.infoView.frame = CGRect(x: 20, y: 15, width: self.viewFrame.width - 40, height: tableView.rowHeight - 30)
+                }) { (animated) in
+                    cell.showViews()
+                }
+                return cell
+            } else {
+                tableView.rowHeight = 500
+                let cell = BookTableViewCell(frame: CGRect(x: 0, y: 0, width: viewFrame.width, height: tableView.rowHeight))
+                cell.infoView.frame = CGRect(x: 20, y: 15, width: viewFrame.width - 40, height: tableView.rowHeight - 30)
+                cell.setData(book: books[indexPath.row])
+                cell.buttonSeeMore.tag = indexPath.row
+                cell.buttonSeeMore.addTarget(self, action: #selector(expandRow(sender:)), for: .touchUpInside)
+                tableView.rowHeight = 200
+                cell.hideViews()
+                UIView.animate(withDuration: 0.5) {
+                    cell.infoView.frame = CGRect(x: 20, y: 15, width: self.viewFrame.width - 40, height: tableView.rowHeight - 30)
+                }
+            }
+        }
+        let reusableCell = tableView.dequeueReusableCell(withIdentifier: BookTableViewCell.ID, for: indexPath) as! BookTableViewCell
+        reusableCell.infoView.frame = CGRect(x: 20, y: 15, width: viewFrame.width - 40, height: tableView.rowHeight - 30)
+        reusableCell.setData(book: books[indexPath.row])
+        reusableCell.buttonSeeMore.tag = indexPath.row
+        reusableCell.buttonSeeMore.addTarget(self, action: #selector(expandRow(sender:)), for: .touchUpInside)
+        return reusableCell
     }
+}
+
+//MARK: Expand an Collapse Row
+extension BooksView {
+    @objc func expandRow (sender: UIButton) {
+        if sender.tag == expandedCellIndex {
+            print("Collapsing row at: \(sender.tag)")
+            expandedCellIndex = nil
+            expand = false
+        } else {
+            print("Expanding row at: \(sender.tag)")
+            expandedCellIndex = sender.tag
+            expand = true
+        }
+        reloadData()
+//        reloadDataWithAnimation()
+//        sender.removeTarget(self, action: #selector(expandRow(sender:)), for: .touchUpInside)
+//        sender.addTarget(self, action: #selector(collapseRow(sender:)), for: .touchUpInside)
+    }
+    
+//    @objc func collapseRow (sender: UIButton) {
+//        print("CollapseRow at: \(sender.tag)")
+//        sender.removeTarget(self, action: #selector(collapseRow(sender:)), for: .touchUpInside)
+//        sender.addTarget(self, action: #selector(expandRow(sender:)), for: .touchUpInside)
+//    }
 }
