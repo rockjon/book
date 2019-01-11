073a0bbc3aaeee825f145a812afe3e8e62f90379
Javier Cruz Santiago <javier.cruz@icomsys.com.mx>
2019-01-11 13:06:11 -0600

    [UPDATE] - Added First Version of CreateViewcontroller, [UPDATE] - Adding code for dismiss keyboard when tapped around at BaseViewController

--- Book/Extensions/ExtensionsUIViewController.swift
+++ Book/Extensions/ExtensionsUIViewController.swift
@@ -21,3 +21,11 @@ class BaseViewController: UIViewController {
         setupLayout()
     }
 }
+
+//MARK: Dismissing Keyboard
+extension BaseViewController {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
+    }
+}
--- Book/Sections/Books/CreateBookViewController.swift
+++ Book/Sections/Books/CreateBookViewController.swift
@@ -13,7 +13,20 @@ class CreateBookViewController: BaseViewController {
     weak var delegate: CreateElementViewDelegate?
     
     var contentView: UIView!
-    var buttonDismiss: UIButton!
+    var imageViewBookCover: UIImageView!
+    var buttonCancel: UIButton!
+    var labelTitle: UILabel!
+    var textFieldTitle: UITextField!
+    var labelAuthor: UILabel!
+    var textFieldAuthor: UITextField!
+    var labelPublished: UILabel!
+    var textFieldPublished: UITextField!
+    var labelPages: UILabel!
+    var textFieldPages: UITextField!
+    var labelDescription: UILabel!
+    var textViewDescription: UITextView!
+    var labelAbstract: UILabel!
+    var textViewAbstract: UITextView!
     var buttonAccept: UIButton!
     
     //MARK: Create View
@@ -24,12 +37,83 @@ class CreateBookViewController: BaseViewController {
         contentView.backgroundColor = .white
         contentView.layer.cornerRadius = 20
         
-        buttonDismiss = UIButton()
-        buttonDismiss.setTitle("Dimiss View", for: .normal)
-        buttonDismiss.setTitleColor(.white, for: .normal)
-        buttonDismiss.backgroundColor = .red
-        buttonDismiss.layer.cornerRadius = 20
-        buttonDismiss.addTarget(self, action: #selector(cancel), for: .touchUpInside)
+        buttonCancel = UIButton()
+        buttonCancel.setImage(UIImage(named: "deleteBtn"), for: .normal)
+        buttonCancel.addTarget(self, action: #selector(cancel), for: .touchUpInside)
+        
+        imageViewBookCover = UIImageView()
+        imageViewBookCover.backgroundColor = UIColor.GRAY
+        imageViewBookCover.layer.cornerRadius = 10
+        imageViewBookCover.contentMode = .scaleToFill
+        
+        labelTitle = UILabel.getCustomLabel(text: "Título:", font: .Bold, size: 15, color: .GRAY, alignment: .left)
+        
+        textFieldTitle = UITextField()
+        textFieldTitle.text = "Book Title"
+        textFieldTitle.layer.cornerRadius = 3
+        textFieldTitle.layer.borderColor = UIColor.GRAY.cgColor
+        textFieldTitle.layer.borderWidth = 0.5
+        textFieldTitle.font = UIFont(name: FontType.Regular.rawValue, size: 15)
+        textFieldTitle.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 1))
+        textFieldTitle.leftViewMode = .always
+        textFieldTitle.delegate = self
+        
+        labelAuthor = UILabel.getCustomLabel(text: "Autor", font: .Bold, size: 15, color: .GRAY, alignment: .left)
+        
+        textFieldAuthor = UITextField()
+        textFieldAuthor.text = "Book Author"
+        textFieldAuthor.layer.cornerRadius = 3
+        textFieldAuthor.layer.borderColor = UIColor.GRAY.cgColor
+        textFieldAuthor.layer.borderWidth = 0.5
+        textFieldAuthor.font = UIFont(name: FontType.Regular.rawValue, size: 15)
+        textFieldAuthor.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 1))
+        textFieldAuthor.leftViewMode = .always
+        textFieldAuthor.delegate = self
+        
+        labelPublished = UILabel.getCustomLabel(text: "Fecha de Publicación:", font: .Bold, size: 15, color: .GRAY, alignment: .left)
+        
+        textFieldPublished = UITextField()
+        textFieldPublished.text = "Published Date"
+        textFieldPublished.layer.cornerRadius = 3
+        textFieldPublished.layer.borderColor = UIColor.GRAY.cgColor
+        textFieldPublished.layer.borderWidth = 0.5
+        textFieldPublished.font = UIFont(name: FontType.Regular.rawValue, size: 15)
+        textFieldPublished.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 1))
+        textFieldPublished.leftViewMode = .always
+        textFieldPublished.delegate = self
+        
+        labelPages = UILabel.getCustomLabel(text: "Páginas", font: .Bold, size: 15, color: .GRAY, alignment: .left)
+        
+        textFieldPages = UITextField()
+        textFieldPages.text = "Book Pages"
+        textFieldPages.layer.cornerRadius = 3
+        textFieldPages.layer.borderColor = UIColor.GRAY.cgColor
+        textFieldPages.layer.borderWidth = 0.5
+        textFieldPages.font = UIFont(name: FontType.Regular.rawValue, size: 15)
+        textFieldPages.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 1))
+        textFieldPages.leftViewMode = .always
+        textFieldPages.keyboardType = .numberPad
+        textFieldPages.delegate = self
+        
+        labelDescription = UILabel.getCustomLabel(text: "Descripción:", font: .Bold, size: 15, color: .GRAY, alignment: .left)
+        
+        textViewDescription = UITextView()
+        textViewDescription.text = "Book Description"
+        textViewDescription.layer.cornerRadius = 3
+        textViewDescription.layer.borderColor = UIColor.GRAY.cgColor
+        textViewDescription.layer.borderWidth = 0.5
+        textViewDescription.font = UIFont(name: FontType.Regular.rawValue, size: 15)
+        textViewDescription.delegate = self
+        
+        labelAbstract = UILabel.getCustomLabel(text: "Resumen", font: .Bold, size: 15, color: .GRAY, alignment: .left)
+        
+        textViewAbstract = UITextView()
+        textViewAbstract.text = "Book Abstract"
+        textViewAbstract.layer.cornerRadius = 3
+        textViewAbstract.layer.borderColor = UIColor.GRAY.cgColor
+        textViewAbstract.layer.borderWidth = 0.5
+        textViewAbstract.font = UIFont(name: FontType.Regular.rawValue, size: 15)
+        textViewAbstract.delegate = self
         
         buttonAccept = UIButton()
         buttonAccept.setTitle("Aceptar", for: .normal)
@@ -41,19 +125,67 @@ class CreateBookViewController: BaseViewController {
     }
     override func addViews() {
         view.addSubview(contentView)
-        contentView.addSubview(buttonDismiss)
+        contentView.addSubview(buttonCancel)
+        contentView.addSubview(imageViewBookCover)
+        contentView.addSubview(labelTitle)
+        contentView.addSubview(textFieldTitle)
+        contentView.addSubview(labelAuthor)
+        contentView.addSubview(textFieldAuthor)
+        contentView.addSubview(labelPublished)
+        contentView.addSubview(textFieldPublished)
+        contentView.addSubview(labelPages)
+        contentView.addSubview(textFieldPages)
+        contentView.addSubview(labelDescription)
+        contentView.addSubview(textViewDescription)
+        contentView.addSubview(labelAbstract)
+        contentView.addSubview(textViewAbstract)
         contentView.addSubview(buttonAccept)
     }
     override func setupLayout() {
-//        contentView.frame = CGRect(x: 20, y: UIApplication.shared.statusBarFrame.height + 20, width: view.frame.width - 40, height: view.frame.height - (UIApplication.shared.statusBarFrame.height + 40))
-        view.addConstraintsWithFormat(format: "V:|-40-[v0]-40-|", views: contentView)
-        view.addConstraintsWithFormat(format: "H:|-20-[v0]-20-|", views: contentView)
+        contentView.frame = CGRect(x: 10, y: (view.frame.height - 550) / 2, width: view.frame.width - 20, height: 550)
         
-        contentView.addConstraintsWithFormat(format: "V:|[v0(40)]", views: buttonDismiss)
-        contentView.addConstraintsWithFormat(format: "H:|[v0]|", views: buttonDismiss)
+        contentView.addConstraintsWithFormat(format: "V:|-10-[v0(30)]", views: buttonCancel)
+        contentView.addConstraintsWithFormat(format: "H:[v0(30)]-10-|", views: buttonCancel)
         
-        contentView.addConstraintsWithFormat(format: "V:[v0(40)]|", views: buttonAccept)
-        contentView.addConstraintsWithFormat(format: "H:|[v0]|", views: buttonAccept)
+        contentView.addConstraintsWithFormat(format: "V:|-10-[v0(120)]", views: imageViewBookCover)
+        contentView.addConstraintsWithFormat(format: "H:|-10-[v0(110)]", views: imageViewBookCover)
+        
+        contentView.addConstraintsWithFormat(format: "V:[v0]-15-[v1(20)]", views: imageViewBookCover,labelTitle)
+        contentView.addConstraintsWithFormat(format: "H:|-10-[v0]-10-|", views: labelTitle)
+        
+        contentView.addConstraintsWithFormat(format: "V:[v0]-5-[v1(20)]", views: labelTitle,textFieldTitle)
+        contentView.addConstraintsWithFormat(format: "H:|-10-[v0]-10-|", views: textFieldTitle)
+        
+        contentView.addConstraintsWithFormat(format: "V:[v0]-10-[v1(20)]", views: textFieldTitle,labelAuthor)
+        contentView.addConstraintsWithFormat(format: "H:|-10-[v0]-10-|", views: labelAuthor)
+        
+        contentView.addConstraintsWithFormat(format: "V:[v0]-5-[v1(20)]", views: labelAuthor,textFieldAuthor)
+        contentView.addConstraintsWithFormat(format: "H:|-10-[v0]-10-|", views: textFieldAuthor)
+        
+        let width = (contentView.frame.width - 30) / 2
+        contentView.addConstraintsWithFormat(format: "V:[v0]-10-[v1(20)]", views: textFieldAuthor,labelPublished)
+        contentView.addConstraintsWithFormat(format: "V:[v0]-10-[v1(20)]", views: textFieldAuthor,labelPages)
+        contentView.addConstraintsWithFormat(format: "H:|-10-[v0(\(width))]", views: labelPublished)
+        contentView.addConstraintsWithFormat(format: "H:[v0(\(width))]-10-|", views: labelPages)
+        
+        contentView.addConstraintsWithFormat(format: "V:[v0]-5-[v1(20)]", views: labelPublished,textFieldPublished)
+        contentView.addConstraintsWithFormat(format: "V:[v0]-5-[v1(20)]", views: labelPages,textFieldPages)
+        contentView.addConstraintsWithFormat(format: "H:|-10-[v0(\(width))]", views: textFieldPublished)
+        contentView.addConstraintsWithFormat(format: "H:[v0(\(width))]-10-|", views: textFieldPages)
+        
+        contentView.addConstraintsWithFormat(format: "V:[v0]-10-[v1(20)]", views: textFieldPublished,labelDescription)
+        contentView.addConstraintsWithFormat(format: "H:|-10-[v0]-10-|", views: labelDescription)
+        
+        contentView.addConstraintsWithFormat(format: "V:[v0]-5-[v1(60)]", views: labelDescription,textViewDescription)
+        contentView.addConstraintsWithFormat(format: "H:|-10-[v0]-10-|", views: textViewDescription)
+        
+        contentView.addConstraintsWithFormat(format: "V:[v0]-10-[v1(20)]", views: textViewDescription,labelAbstract)
+        contentView.addConstraintsWithFormat(format: "H:|-10-[v0]-10-|", views: labelAbstract)
+        
+        contentView.addConstraintsWithFormat(format: "V:[v0]-5-[v1(60)]", views: labelAbstract,textViewAbstract)
+        contentView.addConstraintsWithFormat(format: "H:|-10-[v0]-10-|", views: textViewAbstract)
+        
+        buttonAccept.frame = CGRect(x: 0, y: contentView.frame.height - 50, width: contentView.frame.width, height: 50)
     }
     
     //MARK: LifeCycle
@@ -76,6 +208,33 @@ extension CreateBookViewController {
         print("Making request for add a new book")
         print("OK")
         prepareViewAndDismiss()
+    }
+}
+
+//MARK: TextFieldDelegate Management
+extension CreateBookViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+//MARK: TextviewDelegate Management
+extension CreateBookViewController: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if (text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
     }
 }
 
